50bccc12237da9c1e1798e99017958ce28633527
marufBS <0909maruf@gmail.com>
2025-02-23 16:06:54 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 2;
 
   void _incrementCounter() {
     setState(() {
@@ -53,21 +53,80 @@ class _MyHomePageState extends State<MyHomePage> {
           // crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
-
               color: Colors.white,
-              height: 400,
+              height: 550,
               width: double.infinity,
               margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
               child: ListView(
-                children: List.generate(3,(index)=>Column(
+                children: List.generate(_counter,(index)=>Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("data ${index + 1}"),
+                    Container(
+                      color: Colors.grey,
+                      width: double.infinity,
+                      padding: EdgeInsets.all(5.0),
+                      child: Text("data ${index + 1}",style: TextStyle(backgroundColor: Colors.cyanAccent,color: Colors.black,fontSize: 25),),
+                    ),
                     SizedBox(height: 10), // 10 পিক্সেল গ্যাপ
                   ],
-                ))
+                )).reversed.toList()
+              ),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom:0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      color: Colors.grey,
+                      height: 100,
+                      width: double.infinity,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  contentPadding: const EdgeInsets.only(left: 30.0,top: 15.0,bottom: 15.0),
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.white),
+                                      borderRadius: BorderRadius.circular(30.0)
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.white),
+                                      borderRadius: BorderRadius.circular(30.0)
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  hintText: 'Enter a task'
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 5,),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: const StadiumBorder(),
+                                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12), // Adjust padding
+                              ),
+                              onPressed: (){
+                                setState(() {
+                                  _counter++;
+                                });
+                              },
+                              child: Icon(Icons.add,size: 30,)
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
               ),
             )
-
           ],
         ),
         ),
